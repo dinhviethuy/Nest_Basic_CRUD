a1f6426c2e609ed5e6148d4ad3912a58f78fe774
dinhviethuy <dinhviethuy055@gmail.com>
2025-03-14 21:02:45 +0700

    Custom match decorator

--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,16 +1,23 @@
 import { Exclude, Type } from 'class-transformer'
-import { IsEmail, IsString } from 'class-validator'
+import { IsEmail, IsString, Length } from 'class-validator'
+import { Match } from 'src/shared/decorators/custom-validator.decorator'
 import { SuccessResDTO } from 'src/shared/shared.dto'
 
 export class LoginBodyDTO {
   @IsEmail()
   email: string
   @IsString()
+  @Length(6, 20, {
+    message: 'Password must be between 6 to 20 characters',
+  })
   password: string
 }
 
 export class RegisterBodyDTO extends LoginBodyDTO {
   @IsString()
+  @Match('password', {
+    message: 'Mật khẩu không khớp',
+  })
   confirmPassword: string
   @IsString()
   name: string
