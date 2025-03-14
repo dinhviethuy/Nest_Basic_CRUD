e745144ab2b89ad6ed8950af8ad833bf4065738b
dinhviethuy <dinhviethuy055@gmail.com>
2025-03-14 16:53:14 +0700

    Validation với DTO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // tự động loại bỏ các trường không được định nghĩa trong DTO
+      forbidNonWhitelisted: true, // nếu có trường không được định nghĩa trong DTO thì reject request
+      transform: true, // tự động chuyển các kiểu dữ liệu sang kiểu mong muốn
+      transformOptions: {
+        enableImplicitConversion: true, // cho phép chuyển đổi kiểu dữ liệu ngầm định
+      },
+      exceptionFactory(errors) {
+        return new UnprocessableEntityException(
+          errors.map((error) => ({
+            field: error.property,
+            error: Object.values(error.constraints as any)[0],
+          })),
+        )
+      },
+    }),
+  )
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
